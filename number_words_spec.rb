bc45aca763a990bd091e2671a3aaca61cfcfa7b7
Richie Khoo <evolve2k@gmail.com>
2016-01-30 23:36:13 +1030

    Added next obvious failing spec, not getting nil currently

--- number_words_spec.rb
+++ number_words_spec.rb
@@ -8,4 +8,7 @@ describe "Integer to Word Converter" do
   it "returns zero if the number is 0" do
     expect(0.to_word).to eq("zero")
   end
+  it "returns one if the number is 1" do
+    expect(0.to_word).to eq("one")
+  end
 end
