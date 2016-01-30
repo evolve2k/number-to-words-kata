12ad0dd99c8df82e5d23d948ac00fa193c1824ce
Richie Khoo <evolve2k@gmail.com>
2016-01-31 00:13:49 +1030

    Extended with additional patternless words because stupid english.
    17 is seventeen so 12 should be twoteen, not that hard people.
    We are attached to deeply rooted convention over logic on this one!

--- number_words_spec.rb
+++ number_words_spec.rb
@@ -9,7 +9,13 @@ class Fixnum
     "six"   => 6,
     "seven" => 7,
     "eight" => 8,
-    "nine"  => 9
+    "nine"  => 9,
+    "ten"  => 10,
+    "eleven" => 11,
+    "twelve" => 12,
+    "thirteen" => 13,
+    "twenty" => 20,
+    "thirty" => 30
   }
   def to_word
     WORD_NUMBERS.each do |word, integer|
@@ -29,7 +35,13 @@ describe "Integer to Word Converter" do
     6 => "six",
     7 => "seven",
     8 => "eight",
-    9 => "nine"
+    9 => "nine",
+    10 => "ten",
+    11 => "eleven",
+    12 => "twelve",
+    13 => "thirteen",
+    20 => "twenty",
+    30 => "thirty"
   }.each do |integer, word|
     it "returns #{word} if the number is #{integer}" do
       expect(integer.to_word).to eq(word)
