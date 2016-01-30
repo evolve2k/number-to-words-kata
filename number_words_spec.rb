ffe15ea132dcbd03b479ef342041bafc64d5205c
Richie Khoo <evolve2k@gmail.com>
2016-01-31 00:05:55 +1030

    8 points - Recognising collection - Fleshed out the rest of the collection (*gasp* forgot to record points on creating collection earlier)

--- number_words_spec.rb
+++ number_words_spec.rb
@@ -1,7 +1,15 @@
 class Fixnum
   WORD_NUMBERS = {
-    "zero" => 0,
-    "one" => 1
+    "zero"  => 0,
+    "one"   => 1,
+    "two"   => 2,
+    "three" => 3,
+    "four"  => 4,
+    "five"  => 5,
+    "six"   => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine"  => 9
   }
   def to_word
     WORD_NUMBERS.each do |word, integer|
@@ -15,7 +23,14 @@ describe "Integer to Word Converter" do
   numbers = {
     0 => "zero",
     1 => "one",
-    2 => "two"
+    2 => "two",
+    3 => "three",
+    4 => "four",
+    5 => "five",
+    6 => "six",
+    7 => "seven",
+    8 => "eight",
+    9 => "nine"
   }.each do |integer, word|
     it "returns #{word} if the number is #{integer}" do
       expect(integer.to_word).to eq(word)
