0f82967cee07528456124647abd9727ffe36e607
Richie Khoo <evolve2k@gmail.com>
2016-01-30 23:47:37 +1030

    -3 points - Say everything once and only once. Removed duplication in specs

--- number_words_spec.rb
+++ number_words_spec.rb
@@ -14,11 +14,4 @@ describe "Integer to Word Converter" do
       expect(integer.to_word).to eq(word)
     end
   end
-
-  it "returns zero if the number is 0" do
-    expect(0.to_word).to eq("zero")
-  end
-  it "returns one if the number is 1" do
-    expect(1.to_word).to eq("one")
-  end
 end
